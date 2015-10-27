7c6d9ba5df178e764d656952c8c8041af80f1067
TheGamingSprite <Jake.stanley2004@gmail.com>
2015-10-27 21:18:15 +0000

    Update Command_lolno.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_lolno.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_lolno.java
@@ -5,14 +5,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.TELNET, source = SourceType.BOTH)
 @CommandParameters(description = "Lolno.", usage = "/<command>")
 public class Command_lolno extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender_p.chat("Lolno. Just.. just.. go. just GO AWAY!");
+        sender_p.chat("Lolno. Just.. just.. go. just GO AWAY! BITCH");
         Bukkit.dispatchCommand(sender, "expel");
         return true;
     }
